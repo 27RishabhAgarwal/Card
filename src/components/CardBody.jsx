d16710d48c83931d48d42f46f53347f6d568ac18
Manoj Rawat <rishabhblm@gmail.com>
2024-01-16 04:26:08 +0530

    edits done

--- src/components/CardBody.jsx
+++ src/components/CardBody.jsx
@@ -14,8 +14,8 @@ const CardBody = () => {
                 <span className="material-symbols-outlined">phone</span>
             </div>
             <div className="item-description">
-                <h3>+91 9336117544</h3>
-                <p>Mobile</p>
+                <a href="tel:+919336117544"><h3>+91 9336117544</h3>
+                <p>Mobile</p></a>
             </div>
         </div>
 
@@ -24,7 +24,7 @@ const CardBody = () => {
                 <span className="material-symbols-outlined">email</span>
             </div>
             <div className="item-description">
-                <h3>kedarnathfillingstation@gmail.com</h3>
+            <a href="mailto:kedarnathfillingstation@gmail.com"><h3>kedarnathfillingstation@gmail.com</h3>
                 <p>Email</p>
             </div>
         </div>
@@ -44,20 +44,8 @@ const CardBody = () => {
                 <span className="material-symbols-outlined">location_on</span>
             </div>
             <div className="item-description">
-                <h3>Kedarnath Filling Station<br />Hardoi Road, Kachhauna, Hardoi, Uttar Pradesh 241126</h3>
-                <p>Dealer</p>
-            </div>
-        </div>
-
-        <div className="card-body-item">
-            <div className="item-logo">
-                <span className="material-symbols-outlined">language</span>
-            </div>
-            <div className="item-description">
-                <h3>
-                    <a href="https://maps.app.goo.gl/HvMkwEYdCNstaaJA9">https://maps.app.goo.gl/HvMkwEYdCNstaaJA9</a>
-                </h3>
-                <p>Website</p>
+                <a href="https://maps.app.goo.gl/HvMkwEYdCNstaaJA9"><h3>Kedarnath Filling Station<br />Hardoi Road, Kachhauna, Hardoi, Uttar Pradesh 241126</h3>
+                <p>Dealer</p></a>
             </div>
         </div>
 
